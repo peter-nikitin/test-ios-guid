c6ab5b4b55f65ed99e863a99ceadd22e3dc9aae1
Petr <nikitin.p.94@gmail.com>
2021-06-09 09:41:05 +0300

    Initial Commit

---
+++ Test-Guid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test-Guid
+//
+//  Created by Никитин Петр on 09.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
